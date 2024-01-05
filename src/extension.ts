fba9be7535d90cb00441f751c88de9de339c0004
sena-nana <34237511+sena-nana@users.noreply.github.com>
2024-01-05 11:27:05 +0800

    扩展参数显示范围

--- src/extension.ts
+++ src/extension.ts
@@ -21,20 +21,41 @@ class InlineValuesProvider implements vscode.InlineValuesProvider {
     const inlineValues: vscode.InlineValue[] = [];
     const debugSession = vscode.debug.activeDebugSession;
     if (debugSession) {
-      var activeEditor = vscode.window.activeTextEditor;
-      var symbols = undefined;
+      const activeEditor = vscode.window.activeTextEditor;
+      let symbols = undefined;
       if (activeEditor !== undefined) {
         symbols = await vscode.commands
           .executeCommand<vscode.DocumentSymbol[]>(
             "vscode.executeDocumentSymbolProvider",
             activeEditor.document.uri
           )
-          .then((symbols) => {
-            if (symbols !== undefined) {
-              const list: Record<number, vscode.DocumentSymbol> = {};
-              for (const variable of findVars(symbols)) {
-                console.log(variable.name);
-                list[variable.range.start.line] = variable;
+          .then((symbol) => {
+            if (symbol !== undefined) {
+              const list: Record<number, Array<vscode.DocumentSymbol>> = {};
+              for (const variable of findVars(symbol)) {
+                // Retrieve all ranges in the document where the variable appears
+                const regex = new RegExp(variable.name);
+                for (let i = 0; i < activeEditor.document.lineCount; i++) {
+                  const match = regex.exec(
+                    activeEditor.document.lineAt(i).text
+                  );
+                  if (match === null) {
+                    continue;
+                  }
+                  for (const m of match) {
+                    if (list[i] === undefined) {
+                      list[i] = [];
+                    }
+                    list[i].push({
+                      name: variable.name,
+                      kind: variable.kind,
+                      range: variable.range,
+                      selectionRange: variable.selectionRange,
+                      children: variable.children,
+                      detail: variable.detail,
+                    });
+                  }
+                }
               }
               return list;
             }
@@ -43,32 +64,38 @@ class InlineValuesProvider implements vscode.InlineValuesProvider {
       if (symbols === undefined) {
         return;
       }
+      const symbolcache: Record<string, any> = {};
       for (const line of Object.keys(symbols || {}) as unknown as number[]) {
-        const symbol = symbols[line];
+        const symbolinline = symbols[line];
         if (line > context.stoppedLocation.start.line) {
           break;
         }
-        const symbolRange = new vscode.Range(
-          new vscode.Position(
-            symbol.range.start.line,
-            symbol.range.start.character
-          ),
-          new vscode.Position(symbol.range.end.line, symbol.range.end.character)
-        );
-        const stackFrame = context.frameId
-          ? await debugSession
-              .customRequest("evaluate", {
-                frameId: context.frameId,
-                expression: symbol.name,
-                context: "repl",
-              })
-              .then((result) => result.result)
-          : undefined;
-        const hint = new vscode.InlineValueText(
-          symbolRange,
-          symbol.name + " = " + stackFrame
-        );
-        inlineValues.push(hint);
+        for (const symbol of symbolinline) {
+          const symbolRange = new vscode.Range(
+            new vscode.Position(Number(line), symbol.range.start.character),
+            new vscode.Position(Number(line), symbol.range.end.character)
+          );
+          let stackFrame: any;
+          if (symbolcache[symbol.name] !== undefined) {
+            stackFrame = symbolcache[symbol.name];
+          } else {
+            stackFrame = context.frameId
+              ? await debugSession
+                  .customRequest("evaluate", {
+                    frameId: context.frameId,
+                    expression: symbol.name,
+                    context: "variables",
+                  })
+                  .then((result) => result)
+              : undefined;
+            symbolcache[symbol.name] = stackFrame;
+          }
+          const hint = new vscode.InlineValueText(
+            symbolRange,
+            symbol.name + ": " + stackFrame.type + " = " + stackFrame.result
+          );
+          inlineValues.push(hint);
+        }
       }
     }
 
